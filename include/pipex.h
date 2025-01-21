db5acdbf192b4b53378cda62e626d422cec01c6b
Maxime Gouraud <mgouraud@c1r2p4.42lehavre.fr>
2025-01-21 13:50:52 +0100

    Includes indentation correction

--- include/pipex.h
+++ include/pipex.h
@@ -6,16 +6,16 @@
 /*   By: mgouraud <mgouraud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 13:38:07 by mgouraud          #+#    #+#             */
-/*   Updated: 2025/01/21 13:43:23 by mgouraud         ###   ########.fr       */
+/*   Updated: 2025/01/21 13:50:09 by mgouraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-#include "../libft/libft.h"
+# include "../libft/libft.h"
 
-#include <errno.h>
-#include <sys/wait.h>
+# include <errno.h>
+# include <sys/wait.h>
 
 #endif

5203ff04207bc8391e2c9614e793ccd03bbd8ba9
Maxime Gouraud <mgouraud@c1r2p4.42lehavre.fr>
2025-01-28 16:17:13 +0100

    Pipefd

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mgouraud <mgouraud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 13:32:34 by mgouraud          #+#    #+#             */
-/*   Updated: 2025/01/28 11:09:47 by mgouraud         ###   ########.fr       */
+/*   Updated: 2025/01/28 15:25:04 by mgouraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,31 @@
 
 int main(int argc, char *argv[])
 {
-	int	fd_out;
+	int	pipefd[2];
 	int	fd_in;
+	int	fd_out;
 
 	if (argc != 5)
 	{
 		ft_putendl_fd("Pipex: Wrong arguments count!", 2);
 		exit(EXIT_FAILURE);
 	}
-	else
+	fd_out = open(argv[argc - 1], O_RDWR | O_TRUNC | O_CREAT, 0777);		// * >
+	fd_in = open(argv[1], O_RDONLY);										// * <
+	if (fd_in < 0)
 	{
-		fd_out = open(argv[argc - 1], O_RDWR | O_TRUNC | O_CREAT, 0777);		// * >
-		fd_in = open(argv[1], O_RDONLY);										// * <
-		if (fd_in < 0)
-		{
-			ft_putendl_fd("Pipex: Can't open input file!", 2);
-			// close(fd_out);
-			// exit(EXIT_FAILURE);
-		}
-
-		//TODO Execute all commands one by one
-
-		//! Penser a free
-		//! Penser aux leaks
-		//! Penser a gerer les retours d'erreur des commandes
-		//! Afficher toutes les erreurs possibles
-		//! Penser a VALGRIND
+		ft_putendl_fd("Pipex: Can't open input file!", 2);
+		// close(fd_out);
+		// exit(EXIT_FAILURE);
 	}
 	if (fd_in > 0)
 		close(fd_in);
 	close(fd_out);
 	return 0;
 }
+
+		//! Penser a free
+		//! Penser aux leaks
+		//! Penser a gerer les retours d'erreur des commandes
+		//! Afficher toutes les erreurs possibles
+		//! Penser a VALGRIND

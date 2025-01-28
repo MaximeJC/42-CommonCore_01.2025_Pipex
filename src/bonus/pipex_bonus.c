a5d699d8e272084582e43d8a55215358a603b53e
Maxime Gouraud <mgouraud@c1r2p4.42lehavre.fr>
2025-01-28 10:57:56 +0100

    Move << and >> management in pipex_bonus.c file

--- src/bonus/pipex_bonus.c
+++ src/bonus/pipex_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   pipex_bonus.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mgouraud <mgouraud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/21 13:32:34 by mgouraud          #+#    #+#             */
-/*   Updated: 2025/01/28 10:57:34 by mgouraud         ###   ########.fr       */
+/*   Created: 2025/01/28 10:52:20 by mgouraud          #+#    #+#             */
+/*   Updated: 2025/01/28 10:52:33 by mgouraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,31 @@ int main(int argc, char *argv[])
 	int	fd_out;
 	int	fd_in;
 
-	if (argc != 5)
+	if (argc < 5 || (argc < 6 && !ft_strncmp(argv[2], "here_doc", 8)))
 	{
-		ft_putendl_fd("Pipex: Wrong arguments count!", 2);
+		ft_putendl_fd("Pipex: Not enough arguments!", 2);
 		exit(EXIT_FAILURE);
 	}
 	else
 	{
-		fd_in = open(argv[1], O_RDONLY);								// * <
-		if (fd_in < 0)
+		if (!ft_strncmp(argv[2], "here_doc", 8))
 		{
-			ft_putendl_fd("Pipex: Can't open input file!", 2);
-			exit(EXIT_FAILURE);
+			fd_in = 0;																// * <<
+			fd_out = open(argv[argc - 1], O_RDWR | O_APPEND | O_CREAT, 0777);		// * >>
+			//TODO Gestion << et >>
 		}
-		fd_out = open(argv[argc - 1], O_RDWR | O_TRUNC | O_CREAT, 0777);		// * >
+		else
+		{
+			fd_in = open(argv[1], O_RDONLY);										// * <
+			if (fd_in < 0)
+			{
+				ft_putendl_fd("Pipex: Can't open input file!", 2);
+				exit(EXIT_FAILURE);
+			}
+			fd_out = open(argv[argc - 1], O_RDWR | O_TRUNC | O_CREAT, 0777);		// * >
+			//TODO Get file 2 (argv[argc - 1])
+		}
+		//TODO Check if exist and Get file 1 (argv[1])
 
 		//TODO Execute all commands one by one
 

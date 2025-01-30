0a79247e9a894b9a3db3dffb3b38ec680a373cd1
Maxime Gouraud <mgouraud@c1r2p4.42lehavre.fr>
2025-01-30 13:45:30 +0100

    error_handler - free data

--- src/pipex/utils.c
+++ src/pipex/utils.c
@@ -6,7 +6,7 @@
 /*   By: mgouraud <mgouraud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 09:48:44 by mgouraud          #+#    #+#             */
-/*   Updated: 2025/01/30 11:08:14 by mgouraud         ###   ########.fr       */
+/*   Updated: 2025/01/30 13:39:07 by mgouraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,33 @@
 
 void	data_init(t_pipex **data)
 {
-	(*data)->left_cmd_args = NULL;
-	(*data)->left_cmd_path = NULL;
-	(*data)->right_cmd_args = NULL;
-	(*data)->right_cmd_path = NULL;
+	(*data)->lcmd_args = NULL;
+	(*data)->lcmd_path = NULL;
+	(*data)->rcmd_args = NULL;
+	(*data)->rcmd_path = NULL;
 }
 
-void	error_handler(char *msg)
+void	error_handler(char *msg, t_pipex **data)
 {
 	ft_putendl_fd(msg, 2);
+	if (data != NULL)
+	{
+		if ((*data)->lcmd_path != NULL)
+			free((*data)->lcmd_path);
+		if ((*data)->rcmd_path != NULL)
+			free((*data)->rcmd_path);
+		if ((*data)->lcmd_args != NULL)
+		{
+			free_array((*data)->lcmd_args);
+			free((*data)->lcmd_args);
+		}
+		if ((*data)->rcmd_args != NULL)
+		{
+			free_array((*data)->rcmd_args);
+			free((*data)->rcmd_args);
+		}
+		free(*data);
+	}
 	exit(EXIT_FAILURE);
 }
 

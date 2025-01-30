493da45546a8dde22e6c8089686b60ac4b252090
Maxime Gouraud <mgouraud@c1r2p4.42lehavre.fr>
2025-01-30 12:04:59 +0100

    Add data structure and init function

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: mgouraud <mgouraud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 13:38:07 by mgouraud          #+#    #+#             */
-/*   Updated: 2025/01/30 09:55:55 by mgouraud         ###   ########.fr       */
+/*   Updated: 2025/01/30 11:01:46 by mgouraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,23 @@
 # include <sys/wait.h>
 # include <fcntl.h>
 
+typedef struct s_pipex
+{
+	char	*left_cmd_path;
+	char	**left_cmd_args;
+	char	*right_cmd_path;
+	char	**right_cmd_args;
+}	t_pipex;
+
 // main.c
 
 // parsing.c
+char	**get_env_path(char *envp[]);
 
 // pipex.c
 
 // utils.c
+void	data_init(t_pipex **data);
 void	error_handler(char *msg);
 void	free_array(char ***array);
 
--- src/pipex/utils.c
+++ src/pipex/utils.c
@@ -6,31 +6,40 @@
 /*   By: mgouraud <mgouraud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 09:48:44 by mgouraud          #+#    #+#             */
-/*   Updated: 2025/01/30 09:56:16 by mgouraud         ###   ########.fr       */
+/*   Updated: 2025/01/30 11:08:14 by mgouraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+void	data_init(t_pipex **data)
+{
+	(*data)->left_cmd_args = NULL;
+	(*data)->left_cmd_path = NULL;
+	(*data)->right_cmd_args = NULL;
+	(*data)->right_cmd_path = NULL;
+}
+
 void	error_handler(char *msg)
 {
 	ft_putendl_fd(msg, 2);
+	exit(EXIT_FAILURE);
 }
 
-void	free_array(char ***array)
+void	free_array(char **array)
 {
 	int	i;
 	int	size;
 
 	i = 0;
 	size = 0;
-	while (*array[size] != NULL)
+	while (array[size] != NULL)
 		size++;
 	while (i < size)
 	{
-		free(*array[i]);
+		free(array[i]);
 		i++;
 	}
-	free(*array);
-	return;
+	free(array);
+	return ;
 }

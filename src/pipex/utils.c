e8af2381da941714cbc9249fc0ba10aa53aeaab5
Maxime Gouraud <mgouraud@c1r2p4.42lehavre.fr>
2025-02-04 11:55:42 +0100

    Add end_programm function - Similar to error_handler frees, but without error finish

--- src/pipex/utils.c
+++ src/pipex/utils.c
@@ -6,7 +6,7 @@
 /*   By: mgouraud <mgouraud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 09:48:44 by mgouraud          #+#    #+#             */
-/*   Updated: 2025/02/04 10:48:41 by mgouraud         ###   ########.fr       */
+/*   Updated: 2025/02/04 11:30:15 by mgouraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,4 +41,22 @@ void	error_handler(char *msg, t_pipex **data, char **env_paths, int out)
 			ft_strtab_free(env_paths);
 		exit(EXIT_FAILURE);
 	}
+}
+
+void	end_program(t_pipex **data, char **env_paths)
+{
+	if (data != NULL)
+	{
+		if ((*data)->lcmd_path != NULL)
+			free((*data)->lcmd_path);
+		if ((*data)->rcmd_path != NULL)
+			free((*data)->rcmd_path);
+		if ((*data)->lcmd_args != NULL)
+			ft_strtab_free((*data)->lcmd_args);
+		if ((*data)->rcmd_args != NULL)
+			ft_strtab_free((*data)->rcmd_args);
+		free(*data);
+	}
+	if (env_paths != NULL)
+		ft_strtab_free(env_paths);
 }

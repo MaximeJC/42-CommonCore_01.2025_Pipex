b7ab44268caf3a771e7f790b60a32072a206a0d7
Maxime Gouraud <mgouraud@c1r2p4.42lehavre.fr>
2025-02-04 11:52:54 +0100

    Norm corrections

--- src/bonus/pipex_bonus.c
+++ src/bonus/pipex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mgouraud <mgouraud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 10:52:20 by mgouraud          #+#    #+#             */
-/*   Updated: 2025/01/30 15:22:43 by mgouraud         ###   ########.fr       */
+/*   Updated: 2025/02/04 11:42:55 by mgouraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,19 +26,21 @@
 	{
 		if (!ft_strncmp(argv[2], "here_doc", 8))
 		{
-			fd_in = 0;																// * <<
-			fd_out = open(argv[argc - 1], O_RDWR | O_APPEND | O_CREAT, 0777);		// * >>
+			fd_in = 0;								// * <<
+			fd_out = open(argv[argc - 1], O_RDWR | O_APPEND | O_CREAT, 0777);
+					// * >>
 			//TODO Gestion << et >>
 		}
 		else
 		{
-			fd_in = open(argv[1], O_RDONLY);										// * <
+			fd_in = open(argv[1], O_RDONLY);		// * <
 			if (fd_in < 0)
 			{
 				ft_putendl_fd("Pipex: Can't open input file!", 2);
 				exit(EXIT_FAILURE);
 			}
-			fd_out = open(argv[argc - 1], O_RDWR | O_TRUNC | O_CREAT, 0777);		// * >
+			fd_out = open(argv[argc - 1], O_RDWR | O_TRUNC | O_CREAT, 0777);
+					// * >
 			//TODO Get file 2 (argv[argc - 1])
 		}
 		//TODO Check if exist and Get file 1 (argv[1])

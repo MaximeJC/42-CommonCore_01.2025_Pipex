f0f52ba302eed3b8289a7b40c3dca471d3056051
Maxime Gouraud <mgouraud@c1r2p4.42lehavre.fr>
2025-02-05 15:06:37 +0100

    PIpe working !

--- src/pipex/main.c
+++ src/pipex/main.c
@@ -6,7 +6,7 @@
 /*   By: mgouraud <mgouraud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 13:32:34 by mgouraud          #+#    #+#             */
-/*   Updated: 2025/02/04 16:17:08 by mgouraud         ###   ########.fr       */
+/*   Updated: 2025/02/05 15:03:59 by mgouraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,11 @@
 int	main(int argc, char const *argv[], char *envp[])
 {
 	char	**env_paths;
-	// int		pipefd[2];
+	int		pipefd[2];
+	int		pid1;
+	int		pid2;
+	int		fd_in;
+	int		fd_out;
 	t_pipex	*data;
 
 	if (argc < 5)
@@ -32,10 +36,60 @@ int	main(int argc, char const *argv[], char *envp[])
 	get_cmd_path(&(data->lcmd_path), data->lcmd_args, &data, env_paths);
 	get_cmd_path(&(data->rcmd_path), data->rcmd_args, &data, env_paths);
 	// TODO Add verif si rien trouve => data->lcmd_path uninitialized (pour commande suivante)
+	if (pipe(pipefd) == -1)
+		error_handler(ERR_PIPE, &data, env_paths, 1);
+	fd_in = open(argv[1], O_RDONLY);
+	if (fd_in < 0)
+		error_handler(ERR_INFILE, &data, env_paths, 0);
+	fd_out = open(argv[4], O_RDWR | O_TRUNC | O_CREAT, 0777);
+	if (fd_out < 0)
+		error_handler(ERR_OUTFILE, &data, env_paths, 0);
 	if (data->lcmd_path == NULL)
 		error_handler(ERR_CMD_NOT_FOUND, &data, env_paths, 0);
+	else if(fd_in >= 0)
+	{
+		pid1 = fork();
+		if (pid1 < 0)
+			error_handler(ERR_FORK, &data, env_paths, 0);
+		else if (pid1 == 0)
+		{
+			dup2(fd_in, STDIN_FILENO);
+			dup2(pipefd[1], STDOUT_FILENO);
+			close(fd_in);
+			close(fd_out);
+			close(pipefd[0]);
+			close(pipefd[1]);
+			if (execve(data->lcmd_path, data->lcmd_args, NULL) == -1)
+				error_handler(ERR_FORK, &data, env_paths, 0);
+			end_program(&data, env_paths);
+		}
+	}
 	if (data->rcmd_path == NULL)
 		error_handler(ERR_CMD_NOT_FOUND, &data, env_paths, 0);
+	else if (fd_out >= 0)
+	{
+		pid2 = fork();
+		if (pid2 < 0)
+			error_handler(ERR_FORK, &data, env_paths, 0);
+		else if (pid2 == 0)
+		{
+			dup2(pipefd[0], STDIN_FILENO);
+			dup2(fd_out, STDOUT_FILENO);
+			close(fd_in);
+			close(fd_out);
+			close(pipefd[0]);
+			close(pipefd[1]);
+			if (execve(data->rcmd_path, data->rcmd_args, NULL) == -1)
+				error_handler(ERR_FORK, &data, env_paths, 0);
+			end_program(&data, env_paths);
+		}
+	}
+	close(pipefd[0]);
+	close(pipefd[1]);
+	close(fd_in);
+	close(fd_out);
+	waitpid(-1, NULL, 0);
+	waitpid(-1, NULL, 0);
 	end_program(&data, env_paths);
 }
 

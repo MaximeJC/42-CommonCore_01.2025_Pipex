788a0dd601e62c48d3c08b71741e0f62348bfe2b
Maxime Gouraud <mgouraud@c1r2p4.42lehavre.fr>
2025-02-12 16:36:55 +0100

    Forking in progress - Commands working but not pipes yet

--- src/pipex_bonus/pipex_bonus.c
+++ src/pipex_bonus/pipex_bonus.c
@@ -6,80 +6,110 @@
 /*   By: mgouraud <mgouraud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 09:56:13 by mgouraud          #+#    #+#             */
-/*   Updated: 2025/02/12 15:02:58 by mgouraud         ###   ########.fr       */
+/*   Updated: 2025/02/12 16:33:40 by mgouraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex_bonus.h"
 
-/* void	first_cmd_forking(t_pipex **data,	char **env_paths, int pipefd[],
-	int files_fd[])
+/* void	first_cmd_forking(t_pipex **data, int cmd_index)
 {
-	int	pid1;
+	int	pid;
 
-	if ((*data)->lcmd_path == NULL)
-		error_handler(ERR_CMD_NOT_FOUND, data, env_paths, 0);
-	else if (files_fd[0] >= 0)
+	if ((*data)->cmd_path == NULL)
+		error_handler(ERR_CMD_NOT_FOUND, data,0);
+	else if ((*data)->files_fd[0] >= 0)
 	{
-		pid1 = fork();
-		if (pid1 < 0)
-			error_handler(ERR_FORK, data, env_paths, 0);
-		else if (pid1 == 0)
+		pid = fork();
+		if (pid < 0)
+			error_handler(ERR_FORK, data, 0);
+		else if (pid == 0)
 		{
-			dup2(files_fd[0], STDIN_FILENO);
-			dup2(pipefd[1], STDOUT_FILENO);
-			close_fds(pipefd, files_fd[0], files_fd[1]);
-			if (execve((*data)->lcmd_path, (*data)->lcmd_args, NULL) == -1)
-				error_handler(ERR_FORK, data, env_paths, 0);
-			end_program(data, env_paths);
+			dup2((*data)->files_fd[0], STDIN_FILENO);
+			dup2((*data)->pipes[cmd_index][1], STDOUT_FILENO);
+			if (execve((*data)->cmd_path, (*data)->cmd_args, NULL) == -1)
+				error_handler(ERR_FORK, data, 0);
+			end_program(data, 0);
 		}
 	}
 }
 
-void	mid_cmd_forking(t_pipex **data,	char **env_paths, int pipefd[],
-	int files_fd[])
+void	mid_cmd_forking(t_pipex **data, int cmd_index)
 {
-	int	pid1;
+	int	pid;
 
-	if ((*data)->lcmd_path == NULL)
-		error_handler(ERR_CMD_NOT_FOUND, data, env_paths, 0);
-	else if (files_fd[0] >= 0)
+	if ((*data)->cmd_path == NULL)
+		error_handler(ERR_CMD_NOT_FOUND, data,0);
+	else if ((*data)->files_fd[0] >= 0)
 	{
-		pid1 = fork();
-		if (pid1 < 0)
-			error_handler(ERR_FORK, data, env_paths, 0);
-		else if (pid1 == 0)
+		pid = fork();
+		if (pid < 0)
+			error_handler(ERR_FORK, data, 0);
+		else if (pid == 0)
 		{
-			dup2(pipefd[0], STDIN_FILENO);
-			dup2(pipefd[1], STDOUT_FILENO);
-			close_fds(pipefd, files_fd[0], files_fd[1]);
-			if (execve((*data)->lcmd_path, (*data)->lcmd_args, NULL) == -1)
-				error_handler(ERR_FORK, data, env_paths, 0);
-			end_program(data, env_paths);
+			dup2((*data)->pipes[cmd_index][0], STDIN_FILENO);
+			dup2((*data)->pipes[cmd_index][1], STDOUT_FILENO);
+			if (execve((*data)->cmd_path, (*data)->cmd_args, NULL) == -1)
+				error_handler(ERR_FORK, data, 0);
+			end_program(data, 0);
 		}
 	}
 }
 
-void	last_cmd_forking(t_pipex **data,	char **env_paths, int pipefd[],
-	int files_fd[])
+void	last_cmd_forking(t_pipex **data, int cmd_index)
 {
-	int	pid2;
+	int	pid;
 
-	if ((*data)->rcmd_path == NULL)
-		error_handler(ERR_CMD_NOT_FOUND, data, env_paths, 0);
-	else if (files_fd[1] >= 0)
+	if ((*data)->cmd_path == NULL)
+		error_handler(ERR_CMD_NOT_FOUND, data,0);
+	else if ((*data)->files_fd[0] >= 0)
 	{
-		pid2 = fork();
-		if (pid2 < 0)
-			error_handler(ERR_FORK, data, env_paths, 0);
-		else if (pid2 == 0)
+		pid = fork();
+		if (pid < 0)
+			error_handler(ERR_FORK, data, 0);
+		else if (pid == 0)
 		{
-			dup2(pipefd[0], STDIN_FILENO);
-			dup2(files_fd[1], STDOUT_FILENO);
-			close_fds(pipefd, files_fd[0], files_fd[1]);
-			if (execve((*data)->rcmd_path, (*data)->rcmd_args, NULL) == -1)
-				error_handler(ERR_FORK, data, env_paths, 0);
-			end_program(data, env_paths);
+			dup2((*data)->pipes[cmd_index][0], STDIN_FILENO);
+			dup2((*data)->files_fd[1], STDOUT_FILENO);
+			if (execve((*data)->cmd_path, (*data)->cmd_args, NULL) == -1)
+				error_handler(ERR_FORK, data, 0);
+			end_program(data, 0);
 		}
 	}
 } */
+
+//int cmd_index, int step)
+void	cmd_forking(t_pipex **data, int cmd_index, int step)
+{
+	int	pid;
+
+	if ((*data)->cmd_path == NULL)
+		error_handler(ERR_CMD_NOT_FOUND, data,0);
+	else if ((*data)->files_fd[0] >= 0)
+	{
+		pid = fork();
+		if (pid < 0)
+			error_handler(ERR_FORK, data, 0);
+		else if (pid == 0)
+		{
+			if (step == 0)
+				dup2((*data)->files_fd[0], STDIN_FILENO);
+			else
+				dup2((*data)->pipes[cmd_index][0], STDIN_FILENO);
+				// dup2((*data)->pipes[0][0], STDIN_FILENO);
+			if (step == 2)
+				dup2((*data)->files_fd[1], STDOUT_FILENO);
+			else
+				dup2((*data)->pipes[cmd_index][1], STDOUT_FILENO);
+				// dup2((*data)->pipes[0][1], STDOUT_FILENO);
+			if ((*data)->files_fd[0] >= 0)
+				close((*data)->files_fd[0]);
+			if ((*data)->files_fd[1] >= 0)
+				close((*data)->files_fd[1]);
+			close_pipes(data);
+			if (execve((*data)->cmd_path, (*data)->cmd_args, NULL) == -1)
+				error_handler(ERR_FORK, data, 0);
+			end_program(data, 0);
+		}
+	}
+}

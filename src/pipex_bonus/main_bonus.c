73bcd24a195e65f5883f2869b6cfe5fbb148ce85
Maxime Gouraud <mgouraud@c1r2p4.42lehavre.fr>
2025-02-13 14:38:15 +0100

    Delay waitpid's to execute every commands simultaneously

--- src/pipex_bonus/main_bonus.c
+++ src/pipex_bonus/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mgouraud <mgouraud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 13:32:34 by mgouraud          #+#    #+#             */
-/*   Updated: 2025/02/13 12:28:25 by mgouraud         ###   ########.fr       */
+/*   Updated: 2025/02/13 14:18:57 by mgouraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@ static void	exec_cmds(t_pipex **data, int argc, char const *argv[],
 int	main(int argc, char const *argv[], char *envp[])
 {
 	t_pipex	*data;
+	int		i;
 
+	i = 0;
 	if (argc > 1 && argc < 6 && ft_strncmp(argv[1], "here_doc", 9) == 0)
 		error_handler(ERR_HD_ARGS, NULL, 1);
 	else if (argc < 5)
@@ -26,8 +28,14 @@ int	main(int argc, char const *argv[], char *envp[])
 	data_init(&data, argv, envp);
 	set_pipes(&data, argc);
 	open_files_fd(&data, argv, argc);
-	errno = 0;
+	if (data->files_fd[0] >= 0)
+	{
+		get_cmd(argv[2 + data->here_doc], &data);
+		cmd_forking(&data, 0, 0, envp);
+	}
 	exec_cmds(&data, argc, argv, envp);
+	while (i++ < argc)
+		waitpid(-1, NULL, 0);
 	end_program(&data, 1);
 }
 
@@ -37,18 +45,11 @@ static void	exec_cmds(t_pipex **data, int argc, char const *argv[],
 	int	i;
 
 	i = 3 + (*data)->here_doc;
-	if ((*data)->files_fd[0] >= 0)
-	{
-		get_cmd(argv[2 + (*data)->here_doc], data);
-		cmd_forking(data, i - 3 - (*data)->here_doc, 0, envp);
-		waitpid(-1, NULL, 0);
-	}
 	close((*data)->pipes[i - 2 - (*data)->here_doc][1]);
 	while (i < argc - 2)
 	{
 		get_cmd(argv[i], data);
 		cmd_forking(data, i - 2 - (*data)->here_doc, 1, envp);
-		waitpid(-1, NULL, 0);
 		close((*data)->pipes[i - 1 - (*data)->here_doc][1]);
 		i++;
 	}
@@ -56,6 +57,5 @@ static void	exec_cmds(t_pipex **data, int argc, char const *argv[],
 	{
 		get_cmd(argv[argc - 2], data);
 		cmd_forking(data, i - 2 - (*data)->here_doc, 2, envp);
-		waitpid(-1, NULL, 0);
 	}
 }
